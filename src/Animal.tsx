d5e0d68492bfc1badf868cb633fc1c1885eec745
Bernardo Bressan <bressan.mattos@estudante.ufjf.br>
2024-12-02 20:41:31 -0300

    adiciona condicional unico

--- src/Animal.tsx
+++ src/Animal.tsx
@@ -6,12 +6,22 @@ interface AnimalProps {
     peso: number;
     extincao: boolean;
 }
-
 export default function({icone, nome, peso, extincao}:AnimalProps){
-    return <div className="animal">
-        <div className="icone">{icone}</div>
-        <div className="nome">{nome}</div>
-        <div className="peso">{peso.toLocaleString()} Kg</div>
-        <div className="extincao">{extincao}</div>
-    </div>
+    if(extincao){
+            return ( <div className="animal">
+                <div className="icone">{icone}</div>
+                <div className="nome">{nome}</div>
+                <div className="peso">{peso.toLocaleString()} Kg</div>
+                {extincao && <div className="extincao">Em extinção!</div>}
+                </div>
+                );
+    } else {
+        return ( <div className="animal">
+            <div className="icone">{icone}</div>
+            <div className="nome">{nome}</div>
+            <div className="peso">{peso.toLocaleString()} Kg</div>
+            <div className="extincao"></div>
+            </div>
+            );
+    }
 }
