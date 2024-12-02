7664c0c9be8aa27b672fe7826f1cae92c9508431
Bernardo Bressan <bressan.mattos@estudante.ufjf.br>
2024-12-02 20:50:04 -0300

    adiciona mais animais

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import './App.css'
 function App() {
   return <div className='app'>
     <Animal icone='🦁' nome='Leão' peso={190} extincao/>
+    <Animal icone='🦓' nome='Zebra' peso={130} extincao/>
+    <Animal icone='🦒' nome='Girafa' peso={1200}/>
   </div>;
 }
 
