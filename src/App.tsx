0f430320a41afbb6eb987dece4b9c3ec2648ddfd
Bernardo <bressan.mattos@estudante.ufjf.br>
2024-12-05 22:11:35 -0300

    adiciona filhos na exibicao

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,24 @@ import Exibicao from './Exibicao';
 
 function App() {
   return <div className='app'>
-    <Exibicao abertura={new Date("2024-12-06T08:00:00.00-03:00")}
-    fechamento={new Date("2024-12-06T12:00:00.00-03:00")} cercado = "A1"/>
-    <Animal icone='🦁' nome='Leão' peso={190} extincao/>
-    <Animal icone='🦒' nome='Girafa' peso={1200} extincao/>
-    <Exibicao abertura={new Date("2024-12-06T08:00:00.00-03:00")}
-    fechamento={new Date("2024-12-06T17:00:00.00-03:00")} cercado = "B1"/>
-    <Animal icone='🦜' nome='Papagaio' peso={0.120}/>
+
+    <Exibicao 
+    abertura={new Date("2024-12-06T08:00:00.00-03:00")}
+    fechamento={new Date("2024-12-06T12:00:00.00-03:00")} 
+    cercado = "A1"
+    >
+      <Animal icone='🦁' nome='Leão' peso={190} extincao/>
+      <Animal icone='🦒' nome='Girafa' peso={1200} extincao/>
+      <Animal icone='🦓' nome='Zebra' peso={120} extincao/>
+    </Exibicao>
+
+    <Exibicao 
+    abertura={new Date("2024-12-06T08:00:00.00-03:00")}
+    fechamento={new Date("2024-12-06T17:00:00.00-03:00")} 
+    cercado = "B1"
+    >
+      <Animal icone='🦜' nome='Papagaio' peso={0.12}/>
+    </Exibicao>
   </div>;
 }
 
